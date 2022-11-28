1d8bc4ee9955952c5e49e012a72d9d9ce339ff09
Max Grötsch <max-groetsch@gmx.de>
2022-11-28 18:52:13 +0100

    naja

---
+++ pyapps/helloworld.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3.10
+print("""
+        Hello World
+        """)
+
+myList = ["A", "B", "C", "D", "E"]
+print(myList)
+
+for i in myList:
+    print(i)
+
+//unchageable
+mySet = {"a","b","c","d","e", "e"}
+print(mySet)
+
+//unchangeabel
+myTupel = ("x", "y", "z", "x")
+print(myTupel)
